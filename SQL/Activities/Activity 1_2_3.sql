e2a1c64e805b13f5bd974a845f52c52a5dab5bfe
divyasoni1ibm <159513817+divyasoni1ibm@users.noreply.github.com>
2024-06-16 15:06:50 +0530

    Add files via upload

---
+++ SQL/Activities/Activity 1_2_3.sql
@@ -0,0 +1,34 @@
+REM   Script: Activity 1_2_3
+REM   Create Table, Describe, Insert, Select
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(20), 
+    salesman_city varchar2(20), 
+    commission int 
+);
+
+DESCRIBE salesman
+
+
+INSERT INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13);
+
+INSERT ALL 
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
+
+-- Show data from the salesman_id and city columns
+SELECT salesman_id, salesman_city FROM salesman;
+
+-- Show data of salesman from Paris
+SELECT * FROM salesman WHERE salesman_city='Paris';
+
+-- Show salesman_id and commission of Paul Adam
+SELECT salesman_id, commission FROM salesman WHERE salesman_name='Paul Adam';
+
+
